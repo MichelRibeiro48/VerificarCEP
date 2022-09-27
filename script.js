38ff335aa32e52286bd85ecfacc9b513d0076eeb
Denis Levy <denis.levi11@gmail.com>
2022-09-26 21:20:26 -0300

    Update padding in main tag

--- script.js
+++ script.js
@@ -1,28 +1,32 @@
 const input = document.querySelector('.form__input');
 const card = document.querySelector('.card');
+
+const showData = (data) => {
+  card.innerHTML = `
+  <p>Cep: ${data.cep}</p>
+  <p>Logradouro: ${data.logradouro}</p>
+  <p>Complemento: ${data.complemento}</p>
+  <p>Bairro: ${data.bairro}</p>
+  <p>Localidade: ${data.localidade}</p>
+  <p>UF: ${data.uf}</p>
+  <p>IBGE: ${data.ibge}</p>
+  <p>GIA: ${data.gia}</p>
+  <p>DDD: ${data.ddd}</p>
+  <p>siafi: ${data.siafi}</p>
+  `;
+};
+
 const getAddress = async (event) => {
   event.preventDefault();
   const CEP = input.value;
 
   if (CEP.length === 8) {
+    card.innerHTML = '<strong>Carregando...</strong>';
     const request = await fetch(`https://viacep.com.br/ws/${CEP}/json/`);
     const data = await request.json();
 
-    card.innerHTML = `
-    <p>Cep: ${data.cep}</p>
-    <p>Logradouro: ${data.logradouro}</p>
-    <p>Complemento: ${data.complemento}</p>
-    <p>Bairro: ${data.bairro}</p>
-    <p>Localidade: ${data.localidade}</p>
-    <p>UF: ${data.uf}</p>
-    <p>IBGE: ${data.ibge}</p>
-    <p>GIA: ${data.gia}</p>
-    <p>DDD: ${data.ddd}</p>
-    <p>siafi: ${data.siafi}</p>
-    `;
+    showData(data);
   } else {
     alert('CEP inválido');
   }
-
-  console.log(CEP);
 };
